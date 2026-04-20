4929b04932b4eb599dfe0554150888d76b3c0553
foxz1n <bryangomes16624@gmail.com>
2026-04-20 16:39:56 -0300

    feat: email value-object

---
+++ src/domain/value-objects/email.vo.ts
@@ -0,0 +1,21 @@
+export class Email {
+    private readonly _value: string;
+
+    private constructor(value: string){
+        this._value = value;
+    }
+
+    static create(value: string): Email {
+        const trimmed = value.trim().toLowerCase();
+        if(!Email.isValid(trimmed)){
+            throw new Error('Invalid email')
+        }
+
+        return new Email(trimmed);
+    }
+
+    private static isValid(value: string): boolean {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        return emailRegex.test(value);
+    }
+}
