d9bcc6893904cc98a289327380346c880fa459b5
foxz1n <bryangomes16624@gmail.com>
2026-04-24 19:11:34 -0300

    feat: menu-item repository

---
+++ src/domain/repositories/menu-item.repository.ts
@@ -0,0 +1,11 @@
+import type { MenuItem } from "../entities/menu-item.entity.js";
+
+export interface MenuItemRepository {
+    createMenuItem(item: MenuItem): Promise<void>;
+    findMenuItemById(id: string): Promise<MenuItem | null>;
+    findMenuItemsByRestaurantId(restaurantId: string): Promise<MenuItem[]>;
+    findMenuIdByRestaurantId(restaurantId: string): Promise<string | null>;
+    createMenu(restaurantId: string): Promise<string>;
+    saveMenuItem(item: MenuItem): Promise<void>;
+    deleteMenuItem(id: string): Promise<void>;
+}
