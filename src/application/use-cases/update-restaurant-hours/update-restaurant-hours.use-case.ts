738a04b848d5c0407c3fac69c69704976dcd862c
foxz1n <bryangomes16624@gmail.com>
2026-04-28 21:03:05 -0300

    feat: update-restaurant-hours use-case

---
+++ src/application/use-cases/update-restaurant-hours/update-restaurant-hours.use-case.ts
@@ -0,0 +1,34 @@
+import type { RestaurantRepository } from "@/domain/repositories/restaurant.repository.js";
+import type { UpdateRestaurantHoursInput } from "./update-restaurant-hours.input.js";
+import { left, right, type Either } from "@/shared/either.js";
+import { ResourceNotFoundError } from "@/domain/errors/resource-not-found.error.js";
+import { NotAllowedError } from "@/domain/errors/not-allowed.error.js";
+import type { UpdateRestaurantHoursOuput } from "./update-restaurant-hours.output.js";
+
+type UpdateRestaurantHoursResult = Either<
+    ResourceNotFoundError | NotAllowedError,
+    UpdateRestaurantHoursOuput
+>
+
+export class UpdateRestaurantHoursUseCase{
+    constructor(
+        private readonly restaurantRepository: RestaurantRepository
+    ){}
+
+    async execute(input: UpdateRestaurantHoursInput): Promise<UpdateRestaurantHoursResult>{
+        const restaurant = await this.restaurantRepository.findById(input.restaurantId);
+        if(!restaurant){
+            return left(new ResourceNotFoundError('Restaurant'));
+        }
+
+        if(restaurant.ownerId.value !== input.ownerId){
+            return left(new NotAllowedError())
+        }
+
+        restaurant.updateBusinessHours(input.businessHours);
+
+        await this.restaurantRepository.save(restaurant);
+
+        return right({ restaurantId: restaurant.id.value });
+    }
+}
